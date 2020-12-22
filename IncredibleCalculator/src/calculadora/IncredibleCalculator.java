3ef4c15135c27aa7276ce7b00b65b0021d3a2c92
Manuel Spinola Tubío <spinola.tuman21@triana.salesianos.edu>
2020-12-22 09:38:23 +0100

    Añadida funcionalidad suma

---
+++ IncredibleCalculator/src/calculadora/IncredibleCalculator.java
@@ -0,0 +1,12 @@
+package calculadora;
+
+public class IncredibleCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   public static int suma(int a, int b) {
+		        return a + b;
+		    }
+	}
+
+}
